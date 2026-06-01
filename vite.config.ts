79a514f0cd867ab0092c9496410e8aa549e0cd62
Kittisak Ma <moonrinnn@gmail.com>
2026-06-01 16:19:40 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	base: "/eye-rest-timer/",
+	server: {
+		allowedHosts: ['rr8j8g-5173.csb.app']
+	}
 });
